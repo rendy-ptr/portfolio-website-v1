e8c2ebe71cbea59499db60cfa227c34245413b0e
Rendy-ptr <rendyp348@gmail.com>
2025-04-03 12:26:43 +0700

    feat: add new content project

--- src/icon/react-icons.tsx
+++ src/icon/react-icons.tsx
@@ -1,5 +1,5 @@
 import { FaJsSquare, FaReact, FaNodeJs } from "react-icons/fa";
-import { SiTypescript, SiMongodb, SiNextdotjs, SiTailwindcss } from "react-icons/si";
+import { SiTypescript, SiMongodb, SiNextdotjs, SiTailwindcss, SiBun } from "react-icons/si";
 
 export const reactIcons = {
   Javascript: FaJsSquare,
@@ -9,4 +9,5 @@ export const reactIcons = {
   Mongodb: SiMongodb,
   NextJs: SiNextdotjs,
   Tailwind: SiTailwindcss,
+  Bun: SiBun,
 };
